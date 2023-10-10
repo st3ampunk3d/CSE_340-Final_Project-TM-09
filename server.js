9045a69b54a857e306d1fd21295ada6f6cf75a2f
mr.beale <j.b.beale2015@gmail.com>
2023-10-09 18:56:35 -0600

    Resolving errors

--- server.js
+++ server.js
@@ -2,25 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const data = require('./db/connect')
-const passport = require('passport')
-const session = require('express-session')
 
-require('./db/passport')(passport)
 
 const port = process.env.port || 8080
 const message = 'Joshua Beale | CSE 341 - Week 4 | Project 2 (Pt 2)<br/><br/>'
 
 app
-    .use(bodyParser.json())
-    .use(session({
-        secret: "pigeon poop",
-        resave: false,
-        saveUninitialized: false,
-        //cookie: {secure: true}
-    }))
-
-    .use(passport.initialize())
-    .use(passport.session())
 
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*')
@@ -34,12 +21,6 @@ app
 app.get('/', (req, res) => {
     res.send(req.session.user !== undefined ? `${message}<span style="color:green;">Logged in as ${req.session.user.userName}</span>` : `${message}<span style="color:red;">Logged Out</span>`)
 })
-app.get('/github/callback', passport.authenticate('github', {
-    failureRedirect: '/api-docs', session: false}),
-    (req, res) => {
-        req.session.user = req.user,
-        res.redirect('/')
-    })
 
 data.initDb((err, data) => {
     if (err) {
